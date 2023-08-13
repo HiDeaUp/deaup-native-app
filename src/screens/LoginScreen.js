2043539dc2b39d1060d1f8a3ae2dc69cd6f6e579
Pierre-Henry Soria <pierre@ph7.me>
2023-08-14 01:01:06 +1000

    Add SignIn/SignUp screens

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+// TODO Rename this component to SignIn
+export default function LoginScreen({ navigation }) {
+    return (
+        <View>
+           <Text>Login Screen</Text> 
+            <Button 
+                onPress={() => navigation.push("Register")} 
+                title="Go to Registration" 
+            />
+        </View>
+    )
+}
---
+++ src/screens/RegistrationScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+// TODO Rename this component to SignUp
+export default function RegistrationScreen() {
+    return (
+        <View>
+            <Text>Registration Screen</Text>
+        </View>
+    )
+}
