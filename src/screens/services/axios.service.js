a81eb1579f59b80c565ecce9841e90ff8fd145da
Pierre-Henry Soria <pierre@ph7.me>
2023-08-14 21:57:21 +1000

    Install Axios and create an Axios Instance service

---
+++ src/screens/services/axios.service.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const baseURL = "http://127.0.0.1:3000";
+
+export const api = (() => axios.create({ baseURL }))();
+
+api.interceptors.response.use(
+  (response) => response,
+  (error, request) => {
+    error.message =
+      error.response?.data?.error ||
+      (error.response?.data?.errors
+        ? JSON.stringify(error.response?.data?.errors)
+        : error.message);
+
+    return Promise.reject(error);
+  }
+);
