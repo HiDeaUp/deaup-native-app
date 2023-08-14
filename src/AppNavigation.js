80466b57fce6f215f42cf539a03817ee8d3b13fd
Pierre-Henry Soria <pierre@ph7.me>
2023-08-14 16:42:25 +1000

    Start using NativeBase for Input/Button

--- src/AppNavigation.js
+++ src/AppNavigation.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/nativ";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/stack";
 
-import LoginScreen from "../screens/LoginScreen";
-import RegistrationScreen from "./RegistrationScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegistrationScreen from "./screens/RegistrationScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="SignUp" component={RegistrationScreen} />
-        </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="SignUp"
+          component={RegistrationScreen}
+          options={{ headerTitle: "" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from "react";
 import AppNavigation from "./AppNavigation";
+import { NativeBaseProvider } from "native-base";
+
+const theme = extendTheme({
+  components: {
+    Button: {
+      defaultProps: {
+        colorScheme: "rose",
+        size: "lg",
+      },
+      Input: {
+        defaultProps: {
+          size: "lg",
+          bg: "white",
+          p: "3",
+          borderWith: 0,
+          _focus: { borderWith: 1, borderColor: "rose.400" },
+        },
+      },
+    },
+  },
+});
 
 export default function App() {
-    return <AppNavigation />;
+  return (
+    <NativeBaseProvider theme={theme}>
+    <AppNavigation />
+    </NativeBaseProvider>
+  );
 }
