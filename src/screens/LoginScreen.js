ce515eb4ee8f869de2d6778cb64cdb3d1f7d1781
Pierre-Henry Soria <pierre@ph7.me>
2023-08-14 18:52:58 +1000

    Add SignIn Form Screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,15 +1,73 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  Box,
+  VStack,
+  HStack,
+  Heading,
+  Text,
+  Button,
+  Pressable,
+  Input,
+  Icon,
+} from "native-base";
+import { AntDesign } from "react-native-vector-icons";
 
 // TODO Rename this component to SignIn
 export default function LoginScreen({ navigation }) {
-    return (
-        <View>
-           <Text>Login Screen</Text> 
-            <Button 
-                onPress={() => navigation.push("SignUp")} 
-                title="Go to Registration" 
-            />
-        </View>
-    )
+  return (
+    <Box h="100%" p={5} justifyContent="center">
+      {/* NativeBase's Vertical Stacks */}
+      <VStack space={2} mb={20} alignSelf="center">
+        <Icon
+          as={AntDesign}
+          name="home"
+          size={12}
+          color="rose.600"
+          alignSelf="center"
+        />
+        <Heading size="3xl" mr={2} color="rose.600">
+          Real Estate
+        </Heading>
+      </VStack>
+
+      <VStack space={2} mb={10}>
+        <Heading size="xl">Sign In</Heading>
+        <Text fontSize="lg" mt={5} bold>
+          Email
+        </Text>
+
+        <Input
+          type="text"
+          rounded="xl"
+          fontSize="lg"
+          p={4}
+          autoCapitalize="none"
+        />
+
+        <Text fontSize="lg" mt={5} bold>
+          Password
+        </Text>
+
+        <Input
+          type="password"
+          rounded="xl"
+          fontSize="lg"
+          p={4}
+          autoCapitalize="none"
+        />
+      </VStack>
+
+      <Button mt={5} rounded="3xl" _text={{ fontWeight: "bold" }}>
+        Login
+      </Button>
+
+      <HStack justifyContent="center" mt={5}>
+        <Text fontSize="lg">Don't have an account?</Text>
+
+        <Pressable ml={3} onPress={() => navigation.navigate("SignUp")}>
+          <Text color="rose.600" fontSize="lg" bold underline>Sign Up</Text>
+        </Pressable>
+      </HStack>
+    </Box>
+  );
 }
