674bddf28c0ec6cbfdecf5a732866fff7dd04c88
Pierre-Henry Soria <pierre@ph7.me>
2023-08-14 19:02:37 +1000

    Add SignUn Registration Screen

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -1,11 +1,69 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React from "react";
+import {
+  Box,
+  VStack,
+  HStack,
+  Heading,
+  Text,
+  Button,
+  Pressable,
+  Input,
+  Icon,
+} from "native-base";
 
 // TODO Rename this component to SignUp
 export default function RegistrationScreen() {
-    return (
-        <View>
-            <Text>Registration Screen</Text>
-        </View>
-    )
+  return (
+    <Box h="100%" p={5} justifyContent="center">
+      {/* NativeBase's Vertical Stacks */}
+      <VStack space={2} mb={20} alignSelf="center">
+        <Icon
+          as={AntDesign}
+          name="home"
+          size={12}
+          color="rose.600"
+          alignSelf="center"
+        />
+        <Heading size="3xl" mr={2} color="rose.600">
+          Real Estate
+        </Heading>
+      </VStack>
+
+      <VStack space={2} mb={10}>
+        <Heading size="xl">Sign Up</Heading>
+        <Text fontSize="lg" mt={5} bold>
+          Email
+        </Text>
+
+        <Input
+          type="text"
+          rounded="xl"
+          fontSize="lg"
+          p={4}
+          autoCapitalize="none"
+        />
+
+        <Text fontSize="lg" mt={5} bold>
+          Password
+        </Text>
+
+        <Input
+          type="password"
+          rounded="xl"
+          fontSize="lg"
+          p={4}
+          autoCapitalize="none"
+        />
+      </VStack>
+
+      <Button
+        mt={5}
+        rounded="3xl"
+        _text={{ fontWeight: "bold", textTransform: "capitalize" }}
+      >
+        Sign Up
+      </Button>
+    </Box>
+  );
 }
