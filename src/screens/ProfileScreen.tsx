9ce3f932109bd9d0ab1be987ffdb54a96c6201ad
Pierre-Henry Soria <pierre@ph7.me>
2023-08-17 08:01:30 +1000

    Add onPress SignOut on button

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -24,7 +24,12 @@ export const ProfileScreen = () => {
       </Heading>
 
       <Box position="absolute" bottom={10} left={0} right={0}>
-        <Button m={5} rounded="3xl" _text={{ fontWeight: "bold" }} onPress={onSignOut}>
+        <Button
+          m={5}
+          rounded="3xl"
+          _text={{ fontWeight: "bold" }}
+          onPress={onSignOut}
+        >
           Sign Out
         </Button>
       </Box>
