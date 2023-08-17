36f2a19306f53deef776f77467ba758b4d489656
Pierre-Henry Soria <pierre@ph7.me>
2023-08-17 22:16:58 +1000

    Rename & move API req to standard code

---
+++ src/helpers/api.helper.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const baseURL = "http://127.0.0.1:3000";
+
+// make API requests easily with Axios
+export const api = (() => axios.create({ baseURL }))();
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    error.message =
+      error.response?.data?.error ||
+      (error.response?.data?.errors
+        ? JSON.stringify(error.response?.data?.errors)
+        : error.message);
+
+    return Promise.reject(error);
+  }
+);
