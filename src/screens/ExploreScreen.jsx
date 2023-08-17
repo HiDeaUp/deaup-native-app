d1e9a052691117c183823aa727ad3534d1c827c2
Pierre-Henry Soria <pierre@ph7.me>
2023-08-17 22:17:21 +1000

    Add Explore screen for properties

--- src/screens/ExploreScreen.jsx
+++ src/screens/ExploreScreen.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { Box } from "native-base";
+import React, { useState } from "react";
+import { Box, FlatList } from "native-base";
+
+import { useQueryClient } from "react-query";
+import { useFetchHouses, FETCH_HOUSES_QUERY_KEY } from "../services/house";
 
 export const ExploreScreen = () => {
+  const queryClient = useQueryClient();
+
+  const [category, setCategory] = useState("");
+  const [search, setSearch] = useState("");
+
+  const { data, isLoading, isFetching } = useFetchHouses({ search, category });
   return (
     <Box h="100%" p={5} bg="white">
-      Explore
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        px={5}
+        flexGrow={1}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+        refreshing={isLoading || isFetching}
+        onRefresh={() => queryClient.refetchQueries([FETCH_HOUSES_QUERY_KEY])}
+      />
     </Box>
   );
 };
