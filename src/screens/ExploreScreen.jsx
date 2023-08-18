9e9deeebb23162ec5e27c6eb5c5b6d2cda4f2043
Pierre-Henry Soria <pierre@ph7.me>
2023-08-18 22:08:51 +1000

    Add Search and Category Select on Explore screen

--- src/screens/ExploreScreen.jsx
+++ src/screens/ExploreScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Box, Text, FlatList } from "native-base";
+import { Box, FlatList, VStack, Input, Select, Icon } from "native-base";
 import { useQueryClient } from "react-query";
+import { AntDesign } from "@expo/vector-icons";
 
 import { useFetchHouses, FETCH_HOUSES_QUERY_KEY } from "../services/house";
 import { HouseCard } from "../components/HouseCard";
@@ -14,7 +15,38 @@ export const ExploreScreen = () => {
   const { data, isLoading, isFetching } = useFetchHouses({ search, category });
 
   return (
-    <Box h="100%" p={5} bg="white">
+    <Box h="100%">
+      <VStack p={5} space={4}>
+        <Input
+          p={3}
+          value={search}
+          onChangeText={setSearch}
+          size="xl"
+          placeholder="Search"
+          InputLeftElement={
+            <Icon
+              as={AntDesign}
+              name="search1"
+              size={4}
+              color="gray.500"
+              ml={3}
+            />
+          }
+        />
+        <Select
+          p={3}
+          mt={1}
+          placeholder="Select a category"
+          accessibilityLabel="Select a category"
+          selectedValue={category}
+          onValueChange={setCategory}
+        >
+          <Select.Item label="ALL" value="" />
+          <Select.Item label="Apartment" value="apartment" />
+          <Select.Item label="Detached House" value="detached" />
+          <Select.Item label="Penthouse" value="penthouse" />
+        </Select>
+      </VStack>
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
