302c860996271f37b290e263a09930b9ed0e40d8
Pierre-Henry Soria <pierre@ph7.me>
2023-08-23 18:46:54 +1000

    Create  to fetch user's iterms

--- src/services/house.service.ts
+++ src/services/house.service.ts
@@ -6,6 +6,7 @@ import { useToken } from "../hooks/token.hook";
 import { HouseQuery, HouseSearch } from "../types/house.type";
 
 export const FETCH_HOUSES_QUERY_KEY = "fetchHouses";
+export const FETCH_OWN_HOUSES_QUERY_KEY = "fetchOwnHouses";
 
 export const useFetchHouses = ({ search, category }: HouseSearch): HouseQuery => {
   const token = useToken();
@@ -20,6 +21,31 @@ export const useFetchHouses = ({ search, category }: HouseSearch): HouseQuery =>
       api.get(`/houses.json`, {
         headers: { authorization: token },
         params: { search, category },
+      }),
+    {
+      select: (houseData) => houseData?.data,
+    }
+  );
+
+  return {
+    house,
+    isLoading,
+    isFetching,
+  };
+};
+
+export const useFetchOwnHouses = (): HouseQuery => {
+  const token = useToken();
+
+  const {
+    data: house,
+    isLoading,
+    isFetching,
+  } = useQuery(
+    [FETCH_OWN_HOUSES_QUERY_KEY, token, token],
+    () =>
+      api.get(`/houses/own.json`, {
+        headers: { authorization: token },
       }),
     {
       select: (houseData) => houseData?.data,
